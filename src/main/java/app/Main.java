ff4eb9856e3a15b4c7a9960751198c644b27edd5
0507ellie <102474138+0507ellie@users.noreply.github.com>
2025-11-17 14:42:31 -0500

    Create Main class with main method

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+import javax.swing.*;
+
+import interface_adapter.filter.FilterController;
+import view.DashboardView;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
