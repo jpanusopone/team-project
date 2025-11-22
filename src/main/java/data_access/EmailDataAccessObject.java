34e1cda03cb7055f8a1d2dd9c29fa3ccdbd01664
sophia <sophia.ning.23@gmail.com>
2025-11-21 22:49:15 -0500

    created EmailDataAccessObject

---
+++ src/main/java/data_access/EmailDataAccessObject.java
@@ -0,0 +1,18 @@
+package data_access;
+
+import entity.Email;
+import use_case.get_pinned_emails.GetPinnedEmailsInputData;
+import use_case.get_pinned_emails.GetPinnedEmailsUserDataAccessInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmailDataAccessObject implements GetPinnedEmailsUserDataAccessInterface {
+
+    private final List<Email> database = new ArrayList<>();
+
+    @Override
+    public List<Email> getPinnedEmails(GetPinnedEmailsInputData inputData) {
+        return null;
+    }
+}
