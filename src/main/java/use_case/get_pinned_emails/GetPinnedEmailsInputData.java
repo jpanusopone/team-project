ccdaf5392142a892cd4f9cf43405112d195c5dfa
sophia <sophia.ning.23@gmail.com>
2025-11-21 22:20:37 -0500

    Working on GetPinnedEmails use case

--- src/main/java/use_case/get_pinned_emails/GetPinnedEmailsInputData.java
+++ src/main/java/use_case/get_pinned_emails/GetPinnedEmailsInputData.java
@@ -6,35 +6,35 @@ import java.time.LocalDateTime;
  * Input Data for the Get Pinned Emails use case.
  */
 public class GetPinnedEmailsInputData {
-    private final String sender;
-    private final String title;
-    private final Double suspicionScore;
-    private final LocalDateTime date;
+//    private final String sender;
+//    private final String title;
+//    private final Double suspicionScore;
+//    private final LocalDateTime date;
 
     public GetPinnedEmailsInputData(String sender,
                                     String title,
                                     Double suspicionScore,
                                     LocalDateTime date) {
 
-        this.sender = sender;
-        this.title = title;
-        this.suspicionScore = suspicionScore;
-        this.date = date;
+//        this.sender = sender;
+//        this.title = title;
+//        this.suspicionScore = suspicionScore;
+//        this.date = date;
     }
 
-    public String getSender() {
-        return sender;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Double getSuspicionScore() {
-        return suspicionScore;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
+//    public String getSender() {
+//        return sender;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public Double getSuspicionScore() {
+//        return suspicionScore;
+//    }
+//
+//    public LocalDateTime getDate() {
+//        return date;
+//    }
 }
