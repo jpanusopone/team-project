c1796194eb125956703ced56fe36a457a322e14d
Hali <233129970+Hali-S@users.noreply.github.com>
2025-11-29 09:51:00 -0500

    Implemented Discord button to join server

--- src/main/java/interface_adapter/filter/FilterController.java
+++ src/main/java/interface_adapter/filter/FilterController.java
@@ -1,6 +1,9 @@
 package interface_adapter.filter;
 
 import javax.swing.*;
+import java.awt.*;
+import java.net.URI;
+
 import use_case.filter.FilterInputBoundary;
 import use_case.filter.FilterInputData;
 import use_case.filter.SortBy;
@@ -12,6 +15,7 @@ import view.DashboardView;
 public class FilterController {
     private final DashboardView view;
     private final FilterInputBoundary filterInteractor;
+    private final String discordInviteLinkURL = "https://discord.gg/FmME2xh7";
 
     public FilterController(DashboardView view, FilterInputBoundary filterInteractor) {
         this.view = view;
@@ -19,8 +23,21 @@ public class FilterController {
 
         // Connect button to filter action
         view.getFilterButton().addActionListener(e -> applyFilter());
+
+        // Connect button to Discord Server Invitation
         view.getDiscordButton().addActionListener(e ->
-                JOptionPane.showMessageDialog(view, "Pretend this opens the Discord server!"));
+//                JOptionPane.showMessageDialog(view, "Pretend this opens the Discord server!"));
+        {
+            // Opens invite link to join Discord server in browser
+            try {
+                Desktop.getDesktop().browse(new URI(discordInviteLinkURL));
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(this.view,
+                        "Unable to open browser.",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        });
     }
 
     /**
